7b0fc8935da1ded8fa6e426bdcb413544702cde9
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-03-14 08:14:08 +0700

    Add files via upload

---
+++ sem2/ClassandObject/Cat.java
@@ -0,0 +1,28 @@
+package practicumpbo.classandobject;
+
+/**
+ * Wafiy Anwarul Hikam / 220605110022 / Praktikum PBO / F
+ *
+ * @author ACER A515-45-R3RR
+ */
+class Cat {
+
+    String name;
+    String ras;
+    int age;
+
+    void eat() {
+        System.out.println("The cat is hungry");
+        System.out.println("The cat is having a meal");
+    }
+
+    void sleep() {
+        System.out.println("The cat is tired");
+        System.out.println("The cat is taking a nap");
+    }
+
+    void meong() {
+        System.out.println("The cat needs attention");
+        System.out.println("Meong meong meong!!!");
+    }
+}
