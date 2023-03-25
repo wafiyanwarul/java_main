15330852902ae1df667e1db667a3a4ae42b509cf
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-03-25 10:58:06 +0700

    Update PoorDogFinal.java

--- sem2/ObjectBehavior/PoorDogFinal.java
+++ sem2/ObjectBehavior/PoorDogFinal.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicumpbo.objectbehavior;
 
 /**
- *
+ * Wafiy Anwarul Hikam / 220605110022 / Praktikum PBO / F
  * @author ACER A515-45-R3RR
  */
+
 public class PoorDog {
 
     private int size;
@@ -35,10 +31,14 @@ class PoorDogTestDrive {
 
     public static void main(String[] args) {
 
-//        ORIGINAL -- ASLI : (TANPA NILAI)
-//        PoorDog one = new PoorDog();
-//        System.out.println("Ukuran anjing : " + one.getSize());
-//        System.out.println("Nama anjing : " + one.getName());
+//        ##########################################################
+//                                                                ##
+//        ORIGINAL -- ASLI : (TANPA NILAI)                        ##
+//        PoorDog one = new PoorDog();                            ##
+//        System.out.println("Ukuran anjing : " + one.getSize()); ##
+//        System.out.println("Nama anjing : " + one.getName());   ##
+//                                                                ##
+//        ##########################################################
 
 //        MODIFIED -- DIMODIFIKASI (DENGAN NILAI & PARAMETER)
         PoorDog one = new PoorDog();
