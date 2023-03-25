b8ac6acea6bf55929addd636ac4ea93ecaf32351
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-03-25 11:11:15 +0700

    Class PoorDog (No value, parameters)

--- sem2/ObjectBehavior/PoorDog.java
+++ sem2/ObjectBehavior/PoorDog.java
@@ -22,10 +22,8 @@ public class PoorDog {
 class PoorDogTestDrive {
 
     public static void main(String[] args) {
-
         PoorDog one = new PoorDog();
         System.out.println("Ukuran anjing : " + one.getSize());
         System.out.println("Nama anjing : " + one.getName());
-
     }
 }
