7589b92ebc0c08eeae45e0af7cad2ef17bc7bb10
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-03-25 10:13:32 +0700

    Add files via upload

---
+++ sem2/GoodDog.java
@@ -0,0 +1,28 @@
+package practicumpbo.objectbehavior;
+
+/**
+ *
+ * @author ACER A515-45-R3RR
+ */
+public class GoodDog {
+
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int s) {
+        size = s;
+    }
+
+    void bark() {
+        if (size > 60) {
+            System.out.println("Wooof! Wooof!");
+        } else if (size > 14) {
+            System.out.println("Ruff! Ruff!");
+        } else {
+            System.out.println("Kaing! Kaing!");
+        }
+    }
+}
