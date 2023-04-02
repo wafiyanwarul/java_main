310dbc469e57497c0c1c417f6a829b638fa254df
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-04-02 12:07:10 +0700

    Superclass Animal Inheritance

--- sem2/Hewan.java
+++ sem2/Hewan.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package practicumpbo.polymorphismandinheritance;
 
 /**
- *
+ * Wafiy Anwarul Hikam / 220605110022? / Praktikum PBO / F
  * @author ACER A515-45-R3RR
  */
 class Animal {
