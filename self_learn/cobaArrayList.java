d66d856118b04dc2bfc08287217669e47cf2eb85
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2023-04-16 11:32:22 +0700

    give an explanation to cobaArrayList.java

--- self_learn/cobaArrayList.java
+++ self_learn/cobaArrayList.java
@@ -1,20 +1,23 @@
 //by Wafiy Anwarul HIkam
 //Simple ArrayList
 
+//load package arraylist
 import java.util.ArrayList;
 
 public class cobaArrayList {
 
     static void declareArray(){
+        
+        //deklarasi variabel arraylist
         ArrayList skincare = new ArrayList<>();
 
         //ini adalah indeks ke-0
         skincare.add(12);
 
         //indeks ke-1 dimulai dari nilai true
-        skincare.add(true);
-        skincare.add(12.000);
-        skincare.add("Cleanser");
+        skincare.add(true);//(boolean) == arraylist dapat menampung berbagai tipe data
+        skincare.add(12.000);//(double)
+        skincare.add("Cleanser");//(String)
         skincare.add("Mosturizer");
         skincare.add("Hydrating Toner");
         skincare.add("Exfoliating Toner");
@@ -29,18 +32,27 @@ public class cobaArrayList {
         skincare.add("Body Wash");
         skincare.add("Body Serum");
 
-        skincare.remove(10);
-        skincare.remove("Shampoo");
+        //dapat menghapus list dengan indeks
+        skincare.remove(10);//menghapus ("Pasta Gigi")
+        
+        //dapat juga menghapus dengan mengetikkan nama listnya
+        skincare.remove("Shampoo");//menghapus list shampoo dari arraylist
 
+        //menampilkan isi dari arraylist
         System.out.println("Yang merupakan skincare yaitu : " + skincare);
+        
+        //menampilkan banyak list secara keseluruhan
         System.out.println("Skincare berisi " + skincare.size() + " item");
 
+        //memberi jarak output
         System.out.println();
 
-        System.out.println(skincare.get(14));
+        //menampilkan list sesuai indeks yang kita inginkan
+        System.out.println(skincare.get(14));//indeks ke-14 ("Body Serum")
     }
 
     public static void main(String[] args) {
+        //memanggil method declareArray();
         declareArray();
     }
 }
